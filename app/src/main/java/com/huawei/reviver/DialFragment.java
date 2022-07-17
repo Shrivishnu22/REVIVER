8f8da6e84e0b97bab4e9902e686addbd97966cf0
Shrivishnu22 <akshyavishnu@gmail.com>
2022-07-17 20:07:43 +0530

    Add emergency dialer and its icon.

--- app/src/main/java/com/huawei/reviver/DialFragment.java
+++ app/src/main/java/com/huawei/reviver/DialFragment.java
@@ -1,20 +1,34 @@
 package com.huawei.reviver;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 
 public class DialFragment extends Fragment {
+
+    private Button button;
+
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_dial, container, false);
 
-
+        button = view.findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(Intent.ACTION_DIAL);
+                intent.setData(Uri.parse("tel:108"));
+                startActivity(intent);
+            }
+        });
         return view;
     }
 }
